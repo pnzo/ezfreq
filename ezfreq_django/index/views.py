9099a6608de8fb1fa58a9f95762c74db52c54bb6
pnzo <kurilkin.a@yandex.ru>
2024-06-17 23:09:03 +0300

    django lesson 3

--- ezfreq_django/index/views.py
+++ ezfreq_django/index/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def index(request):
+    return HttpResponse('Site OLOLOL')
+
+
+def test(request):
+    return HttpResponse('<h1></h1>')
